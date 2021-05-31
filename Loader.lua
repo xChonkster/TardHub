a3b99d6bf440596360f076236c6ee363627310b2
xChonkster <77835350+xChonkster@users.noreply.github.com>
2021-05-31 16:43:48 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,19 @@
-error("...")
+local url = string.format("https://github.com/xChonkster/TardHub/tree/main/Games/%s.lua?raw=true", 
+	(function()
+		local games_meta = {
+			[286090429] = "Arsenal"
+		}
+
+		local game_meta = rawget(games_meta, game.PlaceId)
+
+		if not game_meta then return "Universal" else return game_meta end
+	end)()
+)
+
+local queue_on_teleport = syn and syn.queue_on_teleport or function()
+	warn("the queue_on_teleport function was not found on this exploit")
+end
+
+queue_on_teleport(game:HttpGet("https://github.com/xChonkster/TardHub/blob/main/Loader.lua?raw=true"))
+
+loadstring(game:HttpGet(url))()
